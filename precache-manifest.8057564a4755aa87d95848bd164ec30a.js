005f8b146929132b771906a57113c73c388911b3
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-23 22:53:19 -0500

    Updates

---
+++ precache-manifest.8057564a4755aa87d95848bd164ec30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "af8a926395500af4455c",
+    "url": "/MyResume/static/js/main.3baa13b2.chunk.js"
+  },
+  {
+    "revision": "dfad9e0b40f576c6a3fa",
+    "url": "/MyResume/static/js/2.7d012841.chunk.js"
+  },
+  {
+    "revision": "af8a926395500af4455c",
+    "url": "/MyResume/static/css/main.1fd81b1b.chunk.css"
+  },
+  {
+    "revision": "fc31d9b9df8939ad4e222add7beb1434",
+    "url": "/MyResume/index.html"
+  }
+];
