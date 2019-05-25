c1612d03b2057ae87a126c1cb5dbc47beebd8738
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-25 14:35:23 -0500

    Updates

---
+++ precache-manifest.2cc6e6f1934df46154c19b04436714ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0372240c9eb52bd16c4f27269137ca18",
+    "url": "/MyResume/static/media/lokesh.0372240c.jpeg"
+  },
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "251fbefabeba6ad9c47c",
+    "url": "/MyResume/static/js/main.182a6fd7.chunk.js"
+  },
+  {
+    "revision": "fbfa00a5c1c3ef5ffad6",
+    "url": "/MyResume/static/js/2.2a54cd0c.chunk.js"
+  },
+  {
+    "revision": "251fbefabeba6ad9c47c",
+    "url": "/MyResume/static/css/main.3665abcd.chunk.css"
+  },
+  {
+    "revision": "fbfa00a5c1c3ef5ffad6",
+    "url": "/MyResume/static/css/2.8f5e35e7.chunk.css"
+  },
+  {
+    "revision": "e0bdc1af327959f98a87b779eb034d8f",
+    "url": "/MyResume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87987978678015483f5e36e6f7f219eb.js"
+  "/MyResume/precache-manifest.2cc6e6f1934df46154c19b04436714ad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/MyResume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
