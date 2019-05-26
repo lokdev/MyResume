73d065170d73f1c5f34b36c7cd8fbf9b6c9e7f85
Lokesh <lokdev@gmail.com>
2019-05-26 01:10:42 -0500

    Changed Background Pic

--- src/components/social.js
+++ src/components/social.js
@@ -8,7 +8,7 @@ export default class Social extends React.Component{
                 <a href="https://www.facebook.com/lokdev"><i class="fa fa-facebook fa-3x"></i></a>
                 <a href="https://twitter.com/lokdev"><i class="fa fa-twitter ml-2 fa-3x "></i></a>
                 <a href="mailto:dlokesh@outlook.com"><i class="fa fa-envelope  fa-3x ml-2"></i></a>
-                <a href="./src/components/Lokesh_Resume.pdf"> <i class="fa fa-download fa-3x ml-2"></i></a>
+                <a href="./src/components/Lokesh_Resume.pdf" download> <i class="fa fa-download fa-3x ml-2"></i></a>
                 </div>
         );
     }
