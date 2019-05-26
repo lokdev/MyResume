a2070dd739623e58d0205f5c91bde3854f902f7e
Lokesh <lokdev@gmail.com>
2019-05-26 00:48:31 -0500

    Updates

---
+++ precache-manifest.f964daf7f2b616dc7a3b850afeb3b417.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MyResume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "2b5bab57d0ac7b3eaf83",
+    "url": "/MyResume/static/css/main.6b323328.chunk.css"
+  },
+  {
+    "revision": "c777bb308bc8e9d8e4f8",
+    "url": "/MyResume/static/js/runtime~main.17fc0005.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MyResume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "64514c5cd632944267ed",
+    "url": "/MyResume/static/js/2.8bedd40c.chunk.js"
+  },
+  {
+    "revision": "0372240c9eb52bd16c4f27269137ca18",
+    "url": "/MyResume/static/media/lokesh.0372240c.jpeg"
+  },
+  {
+    "revision": "2b5bab57d0ac7b3eaf83",
+    "url": "/MyResume/static/js/main.8e63439d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MyResume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MyResume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MyResume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "64514c5cd632944267ed",
+    "url": "/MyResume/static/css/2.dbc064da.chunk.css"
+  },
+  {
+    "revision": "3d86f8ea2117d275b4e24495c9ab6703",
+    "url": "/MyResume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyResume/precache-manifest.1d396a20ae115e015efb1ce61788b855.js"
+  "/MyResume/precache-manifest.f964daf7f2b616dc7a3b850afeb3b417.js"
 );
 
 workbox.clientsClaim();
